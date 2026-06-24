7c2276e1540ff0e10daff14bb2f21a5e5addd850
Kettlem Drobinheski <miacwb9@gmail.com>
2026-06-24 12:44:39 -0300

    cronometro

---
+++ script.js
@@ -0,0 +1,21 @@
+let segundos = 0;
+let intervalo = null;
+
+const display = document.getElementById('display');
+
+function iniciar() {
+    if (intervalo) return;
+
+    intervalo ; setInterval(() => {
+        segundos++;
+        display.textContent = segundos;
+        }, 1000);
+}
+
+function parar() {
+    clearInterval(intervalo);
+    intervalo = null;
+}
+
+document.getElementById('btnIniciar').addEventListener('click', iniciar);
+document.getElementById('btnParar').addEventListener('click', parar);
